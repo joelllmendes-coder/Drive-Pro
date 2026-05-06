8b50f75d96c64ee4a078ab96e0bcd83ab4473ca4
joelllmendes-coder <joelllmendes@gmail.com>
2026-05-06 19:35:46 -0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'drive-pro-v2';
+const assets = [
+  './',
+  './index.html',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
